0485f51c2e61db208069f0fda17fb1d132c64afe
https://git.denver.cqg/joshf <joshf@cqg.com>
2018-07-20 19:37:41 -0500

    lots of changes

--- typedUrls.js
+++ typedUrls.js
@@ -31,29 +31,35 @@ function onAnchorClick(event) {
   function buildTypedUrlList(divName) {
     // To look for history items visited in the last week,
     // subtract a week of microseconds from the current time.
-    var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+    var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 30;
     var oneWeekAgo = (new Date).getTime() - microsecondsPerWeek;
     // Track the number of callbacks from chrome.history.getVisits()
     // that we expect to get.  When it reaches zero, we have all results.
     var numRequestsOutstanding = 0;
     chrome.history.search({
         'text': '',              // Return every history item....
-        'startTime': oneWeekAgo  // that was accessed less than one week ago.
+        'startTime': oneWeekAgo,  // that was accessed less than one week ago.
+        'maxResults': 5000
       },
       function(historyItems) {
+//        console.log(historyItems);
         // For each history item, get details on all visits.
         for (var i = 0; i < historyItems.length; ++i) {
-          var url = historyItems[i].url;
+          var url = historyItems[i].url // + " " + historyItems[i].lastVisitTime;
+ //         var lastVisitTime = historyItems[i].lastVisitTime
           var processVisitsWithUrl = function(url) {
+ //           console.log('processVisitsWithUrl');
             // We need the url of the visited item to process the visit.
             // Use a closure to bind the  url into the callback's args.
             return function(visitItems) {
               processVisits(url, visitItems);
+ //             console.log('pVWUrl return function');
             };
           };
           chrome.history.getVisits({url: url}, processVisitsWithUrl(url));
           numRequestsOutstanding++;
         }
+
         if (!numRequestsOutstanding) {
           onAllVisitsProcessed();
         }
@@ -63,16 +69,19 @@ function onAnchorClick(event) {
     var urlToCount = {};
     // Callback for chrome.history.getVisits().  Counts the number of
     // times a user visited a URL by typing the address.
+      var ts = [];
+      var usp = [];
+      var diff = 0;
+      var rtrns = [];
     var processVisits = function(url, visitItems) {
       for (var i = 0, ie = visitItems.length; i < ie; ++i) {
-        // Ignore items unless the user typed the URL.
-        if (visitItems[i].transition != 'typed') {
-          continue;
-        }
         if (!urlToCount[url]) {
           urlToCount[url] = 0;
         }
         urlToCount[url]++;
+ //       ts.push(url + ': ' + moment.unix(visitItems[i].visitTime/1000).format("MMMM Do YYYY, h:mm:ss a"));
+        usp.push(url);
+        ts.push(visitItems[i].visitTime/1000);
       }
       // If this is the final outstanding call to processVisits(),
       // then we have the final results.  Use them to build the list
@@ -81,6 +90,14 @@ function onAnchorClick(event) {
         onAllVisitsProcessed();
       }
     };
+ 
+    // for(var i = 1; i < ts.length; i++) {
+    //   diff = ts[i]-ts[i-1];
+    //   console.log(ts.length);
+    //   rtrns.push(diff);
+    // }
+
+    console.log(ts["0"]);
     // This function is called when we have the final list of URls to display.
     var onAllVisitsProcessed = function() {
       // Get the top scorring urls.
@@ -88,6 +105,9 @@ function onAnchorClick(event) {
       for (var url in urlToCount) {
         urlArray.push(url);
       }
+      // for (var ts in urlToCount) {
+      //   urlArray.push(ts);
+      // }
       // Sort the URLs by the number of times the user typed them.
       urlArray.sort(function(a, b) {
         return urlToCount[b] - urlToCount[a];
