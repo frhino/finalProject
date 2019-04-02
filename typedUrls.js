1a61b499176ca86e156bfa302da9013de2d3e0d0
Josh Freivogel <joshf@cqg.com>
2019-04-01 21:02:23 -0500

    added wheelhouse.gif

--- typedUrls.js
+++ typedUrls.js
@@ -31,29 +31,32 @@ function onAnchorClick(event) {
   function buildTypedUrlList(divName) {
     // To look for history items visited in the last week,
     // subtract a week of microseconds from the current time.
-    var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 30;
-    var oneWeekAgo = (new Date).getTime() - microsecondsPerWeek;
+    var microsecondsPerMonth = 1000 * 60 * 60 * 24 * 30;
+    var oneMonthAgo = (new Date).getTime() - microsecondsPerMonth;
     // Track the number of callbacks from chrome.history.getVisits()
     // that we expect to get.  When it reaches zero, we have all results.
     var numRequestsOutstanding = 0;
     chrome.history.search({
         'text': '',              // Return every history item....
-        'startTime': oneWeekAgo,  // that was accessed less than one week ago.
+        'startTime': oneMonthAgo,  // that was accessed less than one month ago.
         'maxResults': 5000
       },
       function(historyItems) {
-//        console.log(historyItems);
+console.log(historyItems);
+console.log(moment.duration(historyItems[0].lastVisitTime-historyItems[1].lastVisitTime).asMinutes());
+        for (let i = 0; i < historyItems.length-1; i++) {
+          let a = [];
+          a = Math.ceil((historyItems[i].lastVisitTime-historyItems[i+1].lastVisitTime) / 60000);
+          //console.log(i + ": " + a);
+        };
         // For each history item, get details on all visits.
         for (var i = 0; i < historyItems.length; ++i) {
-          var url = historyItems[i].url // + " " + historyItems[i].lastVisitTime;
- //         var lastVisitTime = historyItems[i].lastVisitTime
+          var url = historyItems[i].url 
           var processVisitsWithUrl = function(url) {
- //           console.log('processVisitsWithUrl');
             // We need the url of the visited item to process the visit.
             // Use a closure to bind the  url into the callback's args.
             return function(visitItems) {
               processVisits(url, visitItems);
- //             console.log('pVWUrl return function');
             };
           };
           chrome.history.getVisits({url: url}, processVisitsWithUrl(url));
@@ -66,23 +69,31 @@ function onAnchorClick(event) {
       });
     // Maps URLs to a count of the number of times the user typed that URL into
     // the omnibox.
+
     var urlToCount = {};
     // Callback for chrome.history.getVisits().  Counts the number of
     // times a user visited a URL by typing the address.
-      var ts = [];
+      var ts = [];    
+      var tons = [];
       var usp = [];
       var diff = 0;
       var rtrns = [];
     var processVisits = function(url, visitItems) {
+
+//     console.log(visitItems[0]);
       for (var i = 0, ie = visitItems.length; i < ie; ++i) {
         if (!urlToCount[url]) {
           urlToCount[url] = 0;
         }
         urlToCount[url]++;
+        tons.push(moment.unix(visitItems[i].visitTime/1000).format("MMMM Do YYYY, h:mm:ss a"));
  //       ts.push(url + ': ' + moment.unix(visitItems[i].visitTime/1000).format("MMMM Do YYYY, h:mm:ss a"));
         usp.push(url);
         ts.push(visitItems[i].visitTime/1000);
       }
+      
+
+ //       console.log(tons.length);
       // If this is the final outstanding call to processVisits(),
       // then we have the final results.  Use them to build the list
       // of URLs to show in the popup.
@@ -91,27 +102,16 @@ function onAnchorClick(event) {
       }
     };
  
-    // for(var i = 1; i < ts.length; i++) {
-    //   diff = ts[i]-ts[i-1];
-    //   console.log(ts.length);
-    //   rtrns.push(diff);
-    // }
-    var tons = [];
-    tons = [1, 2, 3, 4, 5, 6, 7, 8 , 9, 10, 11, 12, 13, 14, 15];
-    console.log(tons);
-    console.log(tons[4]);
-    console.log(ts);
+
     // This function is called when we have the final list of URls to display.
     var onAllVisitsProcessed = function() {
-      console.log(ts[0] + " " + ts[1]);
+
       // Get the top scorring urls.
       urlArray = [];
       for (var url in urlToCount) {
         urlArray.push(url);
       }
-      // for (var ts in urlToCount) {
-      //   urlArray.push(ts);
-      // }
+ 
       // Sort the URLs by the number of times the user typed them.
       urlArray.sort(function(a, b) {
         return urlToCount[b] - urlToCount[a];
